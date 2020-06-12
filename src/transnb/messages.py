7c8529ef6bf0ba9f3e6728397181611c8de82497
Andy Herd <40397581+herdingdata@users.noreply.github.com>
2020-06-12 10:14:55 +0100

    substitution strings keep number of templates minimal makes it easy to randomise stuff further down the line

--- src/transnb/messages.py
+++ src/transnb/messages.py
@@ -1,5 +1,30 @@
-MESSAGES = [
-    "Trans men are men.",
-    "Trans women are women.",
-    "Non binary people are non binary.",
-]
+from collections import namedtuple
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Msg:
+    """Template for messages"""
+
+    text: str
+    substitution: tuple = None
+
+
+GENDER_PLURALS = ("men", "women", "people")
+MESSAGES = (
+    Msg("Trans {s} are {s}.", GENDER_PLURALS),
+    Msg("Non binary people are non binary."),
+)
+
+
+def get_all_messages() -> tuple:
+    messages = []
+    for msg_template in MESSAGES:
+        if msg_template.substitution is None:
+            messages.append(msg_template.text)
+        else:
+            substitutions = msg_template.substitution
+            for sub in substitutions:
+                messages.append(msg_template.text.format(s=sub))
+    return tuple(messages)
--- src/transnb/tests/test_all_messages.py
+++ src/transnb/tests/test_all_messages.py
@@ -1,7 +1,7 @@
 import inspect
 import os
 
-from transnb.commands import all
+from transnb.messages import get_all_messages
 
 
 def get_script_dir():
@@ -12,6 +12,14 @@ def test__all_messages__match_expected():
     filepath = os.path.join(get_script_dir(), "expected_message_list.txt")
     with open(filepath, "r") as infile:
         expected_messages = [m for m in infile.read().split("\n")]
-    result_messages = all()
+    result_messages = get_all_messages()
 
-    assert result_messages == expected_messages
+    for idx, expect_msg in enumerate(expected_messages):
+        assert result_messages[idx] == expect_msg
+
+
+def test_all_messages__none_more_than_200chars():
+    result_messages = get_all_messages()
+
+    for msg in result_messages:
+        assert len(msg) <= 200

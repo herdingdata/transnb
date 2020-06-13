fe995518ed2e86736c06fe455240108bff1af0f0
Andy Herd <40397581+herdingdata@users.noreply.github.com>
2020-06-13 17:25:59 +0100

    always post if we receive a -m override

--- src/transnb/commands.py
+++ src/transnb/commands.py
@@ -7,14 +7,18 @@ from transnb.time import do_i_post_a_tweet
 
 
 def _do_tweet(message: str):
-    if do_i_post_a_tweet() is True:
-        if message is None:
-            message = messages.get_random_message()
-        click.echo(f"transnb-tweet:posted:{msg}")
+    if message is None:
+        message = messages.get_random_message()
+        do_a_post = do_i_post_a_tweet()
+    else:
+        # we got a message override so let's just post it regardless of probability
+        do_a_post = True
+    if do_a_post:
         auth = tweepy.OAuthHandler(s.API_KEY, s.API_SECRET_KEY)
         auth.set_access_token(s.ACCESS_TOKEN, s.ACCESS_TOKEN_SECRET)
         api = tweepy.API(auth)
         api.update_status(message)
+        click.echo(f"transnb-tweet:posted:{message}")
     else:
         click.echo("transnb-tweet:skipped tweet")
 
